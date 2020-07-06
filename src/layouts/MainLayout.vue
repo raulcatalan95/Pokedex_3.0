441a338a816bae572ac9a36dbd5bf70eeabf5c01
Raul Catalan <raulcatalan95@gmail.com>
2020-07-06 15:59:17 -0400

    ingreso de modal con caracteristicas de pokemon

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -77,6 +77,7 @@ export default {
 <style lang="scss" scoped>
 .q-header{
 background-color: $negative;
+color: yellow;
 }
 
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,47 @@
 <template>
   <q-page class="index">
-
     <div class="cartas">
-      <q-card class="my-card"  v-for="(poke, i) in pokemones" :key="i"  @click="vermas(i)">
-        <img :src="`https://pokeres.bastionbot.org/images/pokemon/${poke.id}.png`" alt="nada" />
+      <q-card
+        class="my-card"
+        v-for="(poke, i) in pokemones"
+        :key="i"
+        @click="vermas(i)"
+        cursor-pointer
+      >
+        <img
+          :src="`https://pokeres.bastionbot.org/images/pokemon/${poke.id}.png`"
+          alt="nada"
+        />
 
         <q-card-actions align="center">
           <h5>{{ poke.name }}</h5>
         </q-card-actions>
+        <q-tooltip
+          content-class="bg-secondary"
+          content-style="font-size: 16px"
+          :offset="[10, 10]"
+        >
+          Click para Ver Mas
+        </q-tooltip>
       </q-card>
-      </div>
-
-        <div class="paginador q-pa-lg flex flex-center">
-       <q-pagination
-      v-model="current"
-      :max="10"
-      input
-      input-class="text-yellow-1"
-    >
-    </q-pagination>
     </div>
 
-  <q-dialog v-model="card" v-for="(pokemon,i) in InfoPoke" :key="i">
+    <div class="paginador q-pa-lg flex flex-center">
+      <q-pagination
+        v-model="current"
+        :max="10"
+        input
+        input-class="text-yellow-1"
+      >
+      </q-pagination>
+    </div>
+
+    <q-dialog v-model="card">
       <q-layout view="Lhh lpR fff" container class="bg-white">
         <q-header class="bg-negative">
           <q-toolbar>
-            
-            <q-toolbar-title>{{pokemon.name}}</q-toolbar-title>
-           
+            <q-toolbar-title>Pokemon</q-toolbar-title>
+
             <q-btn flat v-close-popup round dense icon="close" />
           </q-toolbar>
         </q-header>
@@ -36,18 +50,37 @@
           <q-toolbar inset>
             <q-toolbar-title>Pokedex</q-toolbar-title>
           </q-toolbar>
-        </q-footer>  
-         <q-page-container class="Modal">
+        </q-footer>
+        <q-page-container class="Modal">
           <q-page padding>
-            
-              <h2>{{pokemon.abilities[0].ability.name}}</h2>
-               <h2>{{pokemon.abilities[1].ability.name}}</h2>
-            
+            <div
+              class="imagenes-poke"
+              v-for="imagen in imagenesPoke"
+              :key="imagen"
+            >
+              <img :src="imagen.img_front" alt="" />
+              <img :src="imagen.img_back" alt="" />
+            </div>
+            <div class="habilidades">
+              <h4>Habilidades</h4>
+              <ul v-for="habilidad in habilidades" :key="habilidad">
+                <li>
+                  <h5>{{ habilidad.habilidad }}</h5>
+                </li>
+              </ul>
+            </div>
+            <div class="tipos">
+              <h4>Tipo</h4>
+              <ul v-for="typo in typoPoke" :key="typo">
+                <li>
+                  <h5>{{ typo.typo }}</h5>
+                </li>
+              </ul>
+            </div>
           </q-page>
-        </q-page-container> 
-          </q-layout>
+        </q-page-container>
+      </q-layout>
     </q-dialog>
- 
   </q-page>
 </template>
 
@@ -58,23 +91,54 @@ export default {
   name: "PageIndex",
   data() {
     return {
+      infopokes: "",
+      typoPoke: [],
+      imagenesPoke: [],
+      habilidades: [],
       urlpoke: [],
-       current: 1,
-      card:false,
-      stars:3,
+      current: 1,
+      card: false,
+      stars: 3
     };
   },
   methods: {
-    vermas(i){
-      this.card=true
-      this.$store.dispatch("SetInfoPoke", i)
+    vermas(i) {
+      this.habilidades = [];
+      this.imagenesPoke = [];
+      (this.typoPoke = []), (this.infopokes = "");
+      this.card = true;
+      this.infopokes = this.pokemones[i];
+
+      //INGRESANDO HABILIDADES
+      this.infopokes.abilities.forEach(e => {
+        this.habilidades.push({
+          habilidad: e.ability.name
+        });
+      });
+
+      //INGRESANDO TYPO
+      this.infopokes.types.forEach(e => {
+        this.typoPoke.push({
+          typo: e.type.name
+        });
+      });
+      //INGRESANDO IMAGENES
+
+      this.imagenesPoke.push({
+        img_front: this.infopokes.sprites.front_default,
+        img_back: this.infopokes.sprites.back_default
+      });
+
+      console.log(this.imagenesPoke);
+      console.log(this.typoPoke);
+      console.log(this.habilidades);
+      // this.$store.dispatch("SetInfoPoke", i)
     }
   },
   computed: {
-    ...mapState(["pokemones","InfoPoke"]),
-  
+    ...mapState(["pokemones", "InfoPoke"])
   },
-  created() {
+  mounted() {
     let datospoke = "";
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/`)
@@ -89,7 +153,6 @@ export default {
       .then(res => {
         this.urlpoke.forEach(e => {
           axios.get(e).then(data => {
-            console.log(data);
             this.$store.dispatch("SetPokemones", data.data);
           });
         });
@@ -104,11 +167,10 @@ export default {
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
-  flex-direction: column; 
- 
+  flex-direction: column;
 }
-.cartas{
-   display: flex;
+.cartas {
+  display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
@@ -120,33 +182,27 @@ export default {
   margin: 5px;
   color: transparent;
   opacity: 0.7;
- background-color: #000;
-  
+  background-color: #000;
 }
 
-.my-card:hover{
- box-shadow: #fff 5px 5px 5px;
+.my-card:hover {
+  box-shadow: #f5f500 5px 5px 5px;
   opacity: 0.9;
   color: #000;
-  background-color: #fff;
-  
- 
+  background-color: #ecec2a;
 }
-.my-card img{
+.my-card img {
   width: 100%;
   max-width: 300px;
   height: 300px;
-  opacity: 1.0 !important;
+  opacity: 1 !important;
 }
 .my-card h5 {
   margin: 0px;
 }
-.paginador{
-  
+.paginador {
   padding: 10px;
-  background-color:  $negative;
-  
-  
+  background-color: $negative;
 }
 .Modal {
   background-image: url(../assets/fondo_poke.jpg);
@@ -155,8 +211,40 @@ export default {
   background-position: center;
   display: flex;
   flex-direction: column;
-  color:#fff;
+  color: #fff;
+}
+.habilidades {
+  background-color: #000;
+  opacity: 0.6;
+  width: 100%;
+}
+.habilidades:hover {
+  background-color: #000;
+ box-shadow: #fff 5px 5px 5px;
+}
+.tipos {
+  background-color: #000;
+  opacity: 0.6;
+  width: 100%;
+}
+.tipos:hover {
+  background-color: #000;
+  box-shadow: #fff 5px 5px 5px;
+}
+.imagenes-poke{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  
+}
+.imagenes-poke img{
+  height: 150px;
+  width: 150px;
  
 }
-
+.imagenes-poke img:hover{
+   height: 200px;
+  width: 200px;
+}
 </style>
